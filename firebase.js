8158a296c6f72625e5b3ceea5b3cb3a08f08797d
53099 <53099@edu.raseko.fi>
2026-05-21 13:55:24 +0300

    a

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-storage.js";
+
+const firebaseConfig = {
+
+    apiKey: "OMA_APIKEY",
+    authDomain: "OMA.firebaseapp.com",
+    projectId: "OMA_ID",
+    storageBucket: "OMA.appspot.com",
+    messagingSenderId: "ID",
+    appId:"APP_ID"
+
+};
