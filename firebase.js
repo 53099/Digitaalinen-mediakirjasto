fe03dedf7b4a228dc240f7075a571d9c185e7213
53099 <53099@edu.raseko.fi>
2026-05-27 09:59:09 +0300

    kirjautuminen ja rekisteröityminen

--- firebase.js
+++ firebase.js
@@ -1,15 +1,31 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-storage.js";
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { getAuth }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
 
-    apiKey: "OMA_APIKEY",
-    authDomain: "OMA.firebaseapp.com",
-    projectId: "OMA_ID",
-    storageBucket: "OMA.appspot.com",
-    messagingSenderId: "ID",
-    appId:"APP_ID"
+    apiKey: "YOUR_API_KEY",
 
+    authDomain:
+    "YOUR_PROJECT.firebaseapp.com",
+
+    projectId:
+    "YOUR_PROJECT_ID",
+
+    storageBucket:
+    "YOUR_PROJECT.appspot.com",
+
+    messagingSenderId:
+    "YOUR_ID",
+
+    appId:
+    "YOUR_APP_ID"
 };
+
+const app =
+    initializeApp(firebaseConfig);
+
+export const auth =
+    getAuth(app);
